4eed51c9687d07c41a9cf307302314865aace244
Alvin Tran <TempestSTorm7258@gmail.com>
2023-10-01 13:14:34 -0400

    More Assets, Path Finding updates
    Read.me descripts Assets I pulled
    
    made pathfinding piped from center so avoid corenrs

--- Scripts/Enemy_AI.gd
+++ Scripts/Enemy_AI.gd
@@ -13,11 +13,16 @@ enum aggro_type{
 @onready var area = $Detector
 
 func _physics_process(delta):
-
+	
+		
 	if(len(detected_entities) < 1): return
 	var current_location =  parent.global_transform.origin
 	detected_entities.sort_custom(func(a,b): return parent.global_position.distance_to(a) > parent.global_position.distance_to(b))
 	target_position = (detected_entities[0] as CollisionShape2D).global_position
+	if(is_target_reached()):
+		parent.velocity  = Vector2.ZERO;
+		return
+		#Attack logic?
 	var next_location = get_next_path_position()
 	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
